36f05b07d6cb03de889ec34817033c1d9504c99a
atbeard <atbeard@me.com>
2020-06-07 14:56:39 -0400

    first commit

---
+++ handleBook.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+Name: <?php echo $_POST["name"]; ?><br>
+Party Size: <?php echo $_POST["gridRadios"]; ?><br/>
+Day: <?php echo $_POST["day"]; ?> <br/>
+Time: <?php echo $_POST["time"]; ?>
+
+</body>
+</html>
