6a1a80f0a557718c184c4fe0e75ca0f0366d0409
atbeard <atbeard@me.com>
2020-06-08 20:10:39 -0400

    Adding Email Address field to form

--- handleBook.php
+++ handleBook.php
@@ -2,6 +2,7 @@
 <body>
 
 Name: <?php echo $_POST["name"]; ?><br>
+Email Address: <?php echo $_POST["email"]; ?><br>
 Party Size: <?php echo $_POST["gridRadios"]; ?><br/>
 Day: <?php echo $_POST["day"]; ?> <br/>
 Time: <?php echo $_POST["time"]; ?>
